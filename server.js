2be88b31c3691f09ff64324219babce1a84cea91
Guilherme <gponciano00@gmail.com>
2024-11-13 16:20:20 -0300

    Ajuste no comando de start

--- server.js
+++ server.js
@@ -59,4 +59,7 @@ app.put('/lista/:id',async (req, res) => {
     res.status(200).json({ message: 'Tarefa excluida com sucesso!'})
  })
 
-app.listen(3000)
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => {
+   console.log(`Servidor rodando na porta ${PORT}`);
+ });
